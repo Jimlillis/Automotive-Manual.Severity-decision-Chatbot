7a3207297a7ed0fda2a1b273fe73f3e676385f2e
Alex <alexandrokosm@gmail.com>
2026-03-20 18:37:47 +0200

    [edit] first fast api

--- incident-bot/insurance-bot-backend/fastAPI.py
+++ incident-bot/insurance-bot-backend/fastAPI.py
@@ -13,7 +13,15 @@ app = FastAPI(title="Insurance AI Bot API")
 # Ρύθμιση του μοντέλου (Gemini 1.5 Flash)
 model = genai.GenerativeModel(
     model_name="gemini-3-flash-preview",
-    system_instruction="Είσαι ένας βοηθός ασφαλιστικής εταιρείας. Εξυπηρετείς ΜΟΝΟ περιπτώσεις Οδικής Βοήθειας (Road Assistance) και Φροντίδας Ατυχήματος (Accident Care). Αν ο χρήστης ζητήσει οτιδήποτε άλλο, απάντησε: 'Ευχαριστώ, εξυπηρετώ μόνο Ατυχήματα και Βλάβες. Εαν προκειται για μια απο τις δυο περιπτώσεις που εξυπηρετεις, απαντησε οτι θα ερθει συντομα τεχνικος στο σημειο χωρις κατι πιο συγκεκριμενο.'"
+    system_instruction=(
+        "Είσαι ένας βοηθός ασφαλιστικής εταιρείας. "
+        "Εξυπηρετείς ΜΟΝΟ περιπτώσεις Οδικής Βοήθειας (Road Assistance) "
+        "και Φροντίδας Ατυχήματος (Accident Care). "
+        "Αν ο χρήστης ζητήσει οτιδήποτε άλλο, απάντησε: "
+        "'Ευχαριστώ, εξυπηρετώ μόνο Ατυχήματα και Βλάβες. "
+        "Εαν προκειται για μια απο τις δυο περιπτώσεις που εξυπηρετεις, "
+        "απαντησε οτι θα ερθει συντομα τεχνικος στο σημειο "
+    )
 )
 
 class ChatRequest(BaseModel):
@@ -32,7 +40,7 @@ async def chat_endpoint(request: ChatRequest):
         # Έλεγχος αν το API Key φορτώθηκε
         api_key = os.getenv("GEMINI_API_KEY")
         if not api_key:
-            print("🔴 ΣΦΑΛΜΑ: Το GEMINI_API_KEY είναι κενό στο .env!")
+            print("ΣΦΑΛΜΑ: Το GEMINI_API_KEY είναι κενό στο .env!")
             return {"reply": "Internal Configuration Error: No API Key", "status": "error"}
 
         # Κλήση Gemini
@@ -40,14 +48,14 @@ async def chat_endpoint(request: ChatRequest):
         
         # Έλεγχος αν η απάντηση μπλοκαρίστηκε από φίλτρα ασφαλείας
         if not response.parts:
-            print("⚠️ Το Gemini μπλόκαρε την απάντηση (Safety Filters).")
+            print(" Το Gemini μπλόκαρε την απάντηση (Safety Filters).")
             return {"reply": "Η ερώτηση μπλοκαρίστηκε από τα φίλτρα ασφαλείας.", "status": "blocked"}
 
-        print(f"✅ Απάντηση Gemini: {response.text}")
+        print(f"Απάντηση Gemini: {response.text}")
         return {"reply": response.text, "status": "success"}
 
     except Exception as e:
-        print(f"❌ CRITICAL ERROR: {str(e)}") # Αυτό θα εμφανιστεί στο Terminal σου!
+        print(f" CRITICAL ERROR: {str(e)}") # Αυτό θα εμφανιστεί στο Terminal σου!
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
